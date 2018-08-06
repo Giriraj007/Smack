7e75db15589cf7451f23cc7ece45657630e2a285
Giriraj <bhatiagiriraj007@gmail.com>
2018-08-06 21:56:45 +0530

    initial commit

---
+++ app/src/main/java/comp/example/ahsimapc/smack/SignupActivity.kt
@@ -0,0 +1,53 @@
+package comp.example.ahsimapc.smack
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_signup.*
+import java.util.*
+
+class SignupActivity : AppCompatActivity() {
+
+
+    var useravatar="profiledefault"
+    var avatarcolor="[0.5,0.5,0.5,1]"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+
+     fun changeImage(view: View)
+     { val random=Random()
+         val imageChoice=random.nextInt(2)
+         val backgroundChoice=random.nextInt(28)
+         if(imageChoice==0)
+         { useravatar="light$backgroundChoice"
+
+         }else
+         {
+             useravatar="dark$backgroundChoice"
+         }
+         val imageid=resources.getIdentifier(useravatar,"drawable",packageName)
+         signup_userimage.setImageResource(imageid)
+
+     }
+
+
+
+    fun generatebackgroundcolor(view: View)
+    {
+      val random=Random()
+        val red=random.nextInt(255)
+        val green =random.nextInt(255)
+        val blue=random.nextInt(255)
+        signup_userimage.setBackgroundColor(Color.rgb(red,green,blue))
+
+
+        val savered=red.toDouble()/255
+        val savegreen=green.toDouble()/255
+        val saveblue=blue.toDouble()/255
+        avatarcolor="[$savered,$savegreen,$saveblue]"
+    }
+}
